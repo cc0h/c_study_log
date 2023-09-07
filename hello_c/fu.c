ca4bb6e3577a4341b7792404608fd4b300c1c9bf
cc0h <792166012@qq.com>
2023-09-07 10:55:13 +0800

    use internal terminal instead external terminal. add max function to fu.c.

--- hello_c/fu.c
+++ hello_c/fu.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+int max(int x, int y)
+{
+    return (x > y) ? x : y;
+}
 
 int main()
 {
-    printf("f u\n");
+    int a, b, sum;
+    scanf("%d,%d",&a, &b);
+    sum = a + b;
+    printf("sum is:%d\n", sum);
+    printf("the bigger number is:%d\n", max(a, b));
     return 0;
-}
+    
+}  
+
